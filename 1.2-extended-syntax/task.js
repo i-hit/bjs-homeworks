c2a71b597c1f1a81b7fb9a68850806764797c8a2
Andrey Semenov <andss82@gmail.com>
2020-06-10 10:03:38 +0300

    task1 done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,34 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+  // код для задачи №1 писать здесь
+  let discriminantis = b ** 2 - 4 * a * c;
+  let result = [];
+
+  if (discriminantis >= 0) {
+    result.push(getFirstRoot(a, b, discriminantis));
+    if (discriminantis > 0) {
+      result.push(getSecondRoot(a, b, discriminantis));
+    }
+  }
+  // return x;
+  return result;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getFirstRoot(a, b, discriminantis) {
+  let root = (-b + Math.sqrt(discriminantis)) / (2 * a);
+  return root;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function getSecondRoot(a, b, discriminantis) {
+  let root = (-b - Math.sqrt(discriminantis)) / (2 * a);
+  return root;
+}
+
+function getAverageMark(marks) {
+  // код для задачи №2 писать здесь
+  // return averageMark;
+}
+
+function askDrink(name, dateOfBirthday) {
+  // код для задачи №3 писать здесь
+  // return result;
 }
