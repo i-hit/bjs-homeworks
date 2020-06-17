28ca93b74a699914c5d8d8cbb9f4866a2e3d9143
Andrey Semenov <andss82@gmail.com>
2020-06-17 21:26:59 +0300

    lesson 2.2 task2 done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
+"use strict";
+
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+  let result = true;
+  const strToLower = String(this).toLowerCase();
+  const strReverse = strToLower.split("").reverse().join("");
+
+  for (let i = 0; i < strToLower.length; i++) {
+    if (strToLower[i] !== strReverse[i]) {
+      result = !result;
+      break;
+    }
+  }
+
+  return result;
+};
+
+//
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  // код для задачи №2 писать здесь
+  let average = 0;
+
+  if (marks.length) {
+    for (let i = 0; i < marks.length; i++) {
+      average += marks[i];
+    }
+
+    average /= marks.length;
+  }
+
+  const roundedAverage = Math.round(average);
+  // return averageMark
+  console.log(average, roundedAverage);
+  return roundedAverage;
 }
 
+//
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  // код для задачи №3 писать здесь
+  // return verdict
 }
