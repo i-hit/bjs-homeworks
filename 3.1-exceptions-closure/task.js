34a0c4a235e1318c68b7caced9c54cce11e20512
Andrey Semenov <andss82@gmail.com>
2020-06-24 03:34:14 +0300

    lesson 3.1 task 1 & 2 done

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -23,3 +23,52 @@ function validateCount(string) {
     return err;
   }
 }
+
+// task 2
+
+class Triangle {
+  constructor(a, b, c) {
+    if ((a + b < c || (a + c) < b || (b + c) < a)) {
+      const err = new Error('Треугольник с такими сторонами не существует');
+      throw err;
+    }
+
+    this.sideA = a;
+    this.sideB = b;
+    this.sideC = c;
+  }
+
+  getPerimeter() {
+    const P = this.sideA + this.sideB + this.sideC;
+    return P;
+  }
+
+  getArea() {
+    const p = this.getPerimeter() / 2;
+    const S = Math.sqrt((p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC)));
+    return +(S.toFixed(3));
+  }
+}
+
+class ErrorTriangle {
+  constructor() {
+    this.err = 'Ошибка! Неправильный треугольник';
+  }
+  getArea() {
+    return this.err;
+  }
+  
+  getPerimeter() {
+    return this.err;
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b,  c);
+    }
+
+  catch(err) {
+    return new ErrorTriangle();
+  }
+}
